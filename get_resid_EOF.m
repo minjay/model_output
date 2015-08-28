49d7d74669f05f7e4e4561d13b66a0c133b7e368
minjay <mjfan@ucar.edu>
2015-08-28 12:20:21 -0600

    get reside EOF

---
+++ get_resid_EOF.m
@@ -0,0 +1,25 @@
+load('svd.mat')
+load('theta_phi.mat')
+
+% plot mean
+plot_pot(reshape(mean_map, size(phi)), phi, theta, 1000)
+
+% plot EOFs
+n_EOF = size(V_L, 2);
+for i = 1:n_EOF
+    figure
+    plot_pot(reshape(double(V_L(:, i)), size(phi)), phi, theta, 1000)
+end
+
+% plot time series
+for i = 1:n_EOF
+    subplot(4, 1, i)
+    plot(T_L(:, i))
+    axis tight
+end
+
+for i = 1:720
+    subplot('Position', [0 0 1 1])
+    plot_pot(reshape(double(r(i, :)), size(phi)), phi, theta, 1000)
+    print(['./plots2/', 'fig', num2str(t)], '-dpng')
+end
