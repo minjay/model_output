1d39cbcd9c7dc6761803002c6b1c71c9d868c175
minjay <mjfan@ucar.edu>
2015-08-28 00:17:07 -0600

    update

--- get_resid_regr.m
+++ get_resid_regr.m
@@ -9,13 +9,13 @@ M = 3;
 
 for t = 1:720
     [coef, resid, y_hat] = SCHA_regr(all_Pot_N{t}, theta, phi, L, M);
-    subplot('position', [0, 0.5 0.45 0.45])
+    subplot('position', [0 0.5 0.45 0.45])
     title(['True, time point ', num2str(t)])
     plot_pot(all_Pot_N{t}, phi, theta, 1000)
-    subplot('position', [0.5, 0.5 0.45 0.45])
+    subplot('position', [0.5 0.5 0.45 0.45])
     title('Fitted')
     plot_pot(reshape(y_hat, size(phi)), phi, theta, 1000)
-    subplot('position', [0, 0 0.45 0.45])
+    subplot('position', [0 0 0.45 0.45])
     title('Residual')
     plot_pot(reshape(resid, size(phi)), phi, theta, 1000)
     
