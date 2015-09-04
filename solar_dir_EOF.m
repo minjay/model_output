260c4fa4065bdbb86c2af9572ea8b08a644e5e2b
minjay <mjfan@ucar.edu>
2015-09-03 23:14:03 -0600

    r_sub

--- solar_dir_EOF.m
+++ solar_dir_EOF.m
@@ -39,6 +39,10 @@ T_L = X*V_L;
 X_L = T_L*V_L';
 r = X-X_L;
 
+r_sub = r(1:720, :);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 direction = {'W', 'SW', 'S', 'SE', 'E', 'NE', 'N', 'NW'};
 loc_x = [0.15 0.25 0.4 0.55 0.65 0.55 0.4 0.25];
 loc_y = [0.4 0.2 0.05 0.2 0.4 0.6 0.75 0.6];
@@ -51,7 +55,7 @@ quiver(0, -1, 0, 2, 0, 'LineWidth', 2, 'color', 'k')
 
 for i = 1:8
     index = strcmp(labels, direction(i));
-    mean_dir = mean(r(index, :), 1);
+    mean_dir = mean(r_sub(index(1:720), :), 1);
     axes('position', [loc_x(i) loc_y(i) 0.25 0.25])
     plot_pot_lite(reshape(mean_dir, size(phi)), phi, theta, 1000)
 end
