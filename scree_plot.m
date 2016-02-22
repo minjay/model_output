10023aa4d0caaad59ff049935da793717732afff
minjay <mjfan@ucar.edu>
2016-02-22 13:02:13 -0800

    change plot style

--- scree_plot.m
+++ scree_plot.m
@@ -3,19 +3,19 @@ load('svd_single')
 n_EOF = 30;
 
 ratio = cumsum(lambda)/sum(lambda);
-plot(ratio(1:n_EOF), 'b.')
+plot(ratio(1:n_EOF), '-mo', 'color', 'b')
 
 load('svd_double')
 
 hold on
 
 ratio = cumsum(lambda)/sum(lambda);
-plot(ratio(1:n_EOF), 'r.')
+plot(ratio(1:n_EOF), '-mo', 'color', 'r')
 
 load('svd_quad')
 
 ratio = cumsum(lambda)/sum(lambda);
-plot(ratio(1:n_EOF), 'k.')
+plot(ratio(1:n_EOF), '-mo', 'color', 'k')
 
 legend('Single', 'Double', 'Quad')
 
